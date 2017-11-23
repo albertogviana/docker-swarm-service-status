ec261ed6e9243f21154dc2dd538e7248913faa7b
Alberto Viana <viana@vesseltracker.com>
2017-11-23 16:43:21 +0100

    Start project

---
+++ service/service.go
@@ -0,0 +1,195 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/client"
+)
+
+// Service struct
+type Service struct {
+	Host         string
+	DockerClient *client.Client
+}
+
+// DeploymentStatus structure
+type DeploymentStatus struct {
+	ID              string `json:",omitempty"`
+	Name            string
+	Err             string              `json:",omitempty"`
+	TaskStatus      []TaskStatus        `json:",omitempty"`
+	Replicas        *uint64             `json:",omitempty"`
+	RunningReplicas int                 `json:",omitempty"`
+	FailedReplicas  int                 `json:",omitempty"`
+	UpdateStatus    *swarm.UpdateStatus `json:",omitempty"`
+}
+
+// TaskStatus structure
+type TaskStatus struct {
+	TaskID       string          `json:",omitempty"`
+	Timestamp    time.Time       `json:",omitempty"`
+	DesiredState swarm.TaskState `json:",omitempty"`
+	State        swarm.TaskState `json:",omitempty"`
+	Message      string          `json:",omitempty"`
+	Err          string          `json:",omitempty"`
+	Image        string          `json:",omitempty"`
+}
+
+// Services defines interfaces with the required methods
+type Services interface {
+	GetService(filter filters.Args) (swarm.Service, error)
+	GetTask(filter filters.Args) ([]swarm.Task, error)
+	GetDeploymentStatus(serviceName string, image string) (DeploymentStatus, error)
+}
+
+// NewService returns a new instance of the Service structure
+func NewService(host, dockerAPIVersion string, defaultHeaders map[string]string) *Service {
+	client, err := client.NewClient(host, dockerAPIVersion, nil, defaultHeaders)
+	if err != nil {
+		fmt.Printf("Something went wrong when tries to connect on the docker host: %s", err.Error())
+	}
+
+	return &Service{
+		host,
+		client,
+	}
+}
+
+// GetService returns swarm.Service struct
+// You will find the available filters on https://docs.docker.com/engine/api/v1.32/#operation/ServiceList
+func (s *Service) GetService(filter filters.Args) (swarm.Service, error) {
+	serviceList, err := s.DockerClient.ServiceList(context.Background(), types.ServiceListOptions{Filters: filter})
+
+	swarmService := swarm.Service{}
+	if err != nil {
+		return swarmService, err
+	}
+
+	for _, service := range serviceList {
+		swarmService = service
+	}
+
+	return swarmService, nil
+}
+
+// GetTask returns the tasks related to a specific service id
+// You will find the available filters on https://docs.docker.com/engine/api/v1.32/#operation/TaskList
+func (s *Service) GetTask(filter filters.Args) ([]swarm.Task, error) {
+	tasks, err := s.DockerClient.TaskList(context.Background(), types.TaskListOptions{Filters: filter})
+
+	if err != nil {
+		return []swarm.Task{}, err
+	}
+
+	return tasks, nil
+}
+
+// GetDeploymentStatus returns the information about a service and it verifies if the tasks are running
+// or for some reason it failed
+func (s *Service) GetDeploymentStatus(serviceName string, image string) (DeploymentStatus, error) {
+	filterService := filters.NewArgs()
+	filterService.Add("name", serviceName)
+	swarmService, err := s.GetService(filterService)
+
+	deploymentStatus := DeploymentStatus{}
+	if err != nil {
+		return deploymentStatus, err
+	}
+
+	deploymentStatus.Name = serviceName
+
+	if swarmService.ID == "" {
+		deploymentStatus.Err = fmt.Sprintf("The %s service was not found in the cluster.", serviceName)
+		return deploymentStatus, nil
+	}
+
+	filterTask := filters.NewArgs()
+	filterTask.Add("service", swarmService.ID)
+
+	swarmTask, err := s.GetTask(filterTask)
+	if err != nil {
+		return deploymentStatus, err
+	}
+
+	deploymentStatus.ID = swarmService.ID
+
+	if s.isImageDeploy(swarmTask, image) == false {
+		deploymentStatus.Err = fmt.Sprintf("The %s image was not deployed or not found in the current tasks running.", image)
+		return deploymentStatus, nil
+	}
+
+	deploymentStatus.Replicas = swarmService.Spec.Mode.Replicated.Replicas
+	deploymentStatus.TaskStatus = s.parseTaskState(swarmTask)
+	deploymentStatus.UpdateStatus = swarmService.UpdateStatus
+
+	deploymentStatus.RunningReplicas, deploymentStatus.FailedReplicas = s.taskStateCount(deploymentStatus, image)
+
+	if deploymentStatus.FailedReplicas > deploymentStatus.RunningReplicas && uint64(deploymentStatus.RunningReplicas) < *deploymentStatus.Replicas {
+		deploymentStatus.Err = fmt.Sprintf("Looks like something went wrong during the deployment, because the %s service failed %d time(s) since last deployment", serviceName, deploymentStatus.FailedReplicas)
+	}
+
+	if deploymentStatus.UpdateStatus != nil && (deploymentStatus.UpdateStatus.State == swarm.UpdateStatePaused || deploymentStatus.UpdateStatus.State == swarm.UpdateStateRollbackCompleted || deploymentStatus.UpdateStatus.State == swarm.UpdateStateRollbackPaused) {
+		deploymentStatus.Err = fmt.Sprintf("Something went wrong during the deployment of the %s service. The error message is: %s", serviceName, deploymentStatus.UpdateStatus.Message)
+	}
+
+	return deploymentStatus, nil
+}
+
+func (s *Service) parseTaskState(swarmTask []swarm.Task) []TaskStatus {
+	taskStatus := []TaskStatus{}
+	for _, task := range swarmTask {
+		ts := TaskStatus{
+			task.ID,
+			task.Status.Timestamp,
+			task.DesiredState,
+			task.Status.State,
+			task.Status.Message,
+			task.Status.Err,
+			task.Spec.ContainerSpec.Image,
+		}
+
+		taskStatus = append(taskStatus, ts)
+	}
+
+	return taskStatus
+}
+
+func (s *Service) isImageDeploy(swarmTask []swarm.Task, image string) bool {
+	imageDeployed := false
+	for _, task := range swarmTask {
+		if s.getImage(task.Spec.ContainerSpec.Image) == image {
+			imageDeployed = true
+		}
+	}
+
+	return imageDeployed
+}
+
+func (s *Service) getImage(image string) string {
+	currentImage := strings.Split(image, "@")
+	return currentImage[0]
+}
+
+func (s *Service) taskStateCount(deploymentStatus DeploymentStatus, image string) (int, int) {
+	runningTaskCount := 0
+	errorTaskCount := 0
+
+	for _, ds := range deploymentStatus.TaskStatus {
+
+		if ds.State == swarm.TaskStateFailed || ds.State == swarm.TaskStateRejected || ds.State == swarm.TaskStateShutdown && ds.DesiredState == swarm.TaskStateShutdown && s.getImage(ds.Image) == image {
+			errorTaskCount = errorTaskCount + 1
+		}
+
+		if ds.State == swarm.TaskStateRunning && ds.DesiredState == swarm.TaskStateRunning && s.getImage(ds.Image) == image {
+			runningTaskCount = runningTaskCount + 1
+		}
+	}
+
+	return runningTaskCount, errorTaskCount
+}
---
+++ service/service_test.go
@@ -0,0 +1,231 @@
+package service
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/swarm"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type ServiceTestSuite struct {
+	suite.Suite
+}
+
+const DockerHost = "unix:///var/run/docker.sock"
+const DockerAPIVersion = "v1.33"
+
+func TestServiceTestSuite(t *testing.T) {
+	createTestServices()
+	suite.Run(t, new(ServiceTestSuite))
+	removeTestServices()
+}
+
+func (s *ServiceTestSuite) Test_NewService_ReturnService() {
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+
+	assert.NotNil(s.T(), service)
+	assert.Equal(s.T(), DockerHost, service.Host)
+}
+
+func (s *ServiceTestSuite) Test_GetService_ReturnSwarmService() {
+	serviceName := "docker-routing-mesh"
+	filterList := filters.NewArgs()
+	filterList.Add("name", serviceName)
+
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	swarmService, err := service.GetService(filterList)
+
+	assert.NoError(s.T(), err)
+	assert.IsType(s.T(), swarm.Service{}, swarmService)
+	assert.Equal(s.T(), serviceName, swarmService.Spec.Name)
+}
+
+func (s *ServiceTestSuite) Test_GetService_ReturnEmptySwarmService_NoServiceFound() {
+	serviceName := "my-service"
+	filterList := filters.NewArgs()
+	filterList.Add("name", serviceName)
+
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	swarmService, err := service.GetService(filterList)
+
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), swarm.Service{}, swarmService)
+}
+
+func (s *ServiceTestSuite) Test_GetService_ReturnError_InvalidFilter() {
+	serviceName := "docker-routing-mesh"
+	filterList := filters.NewArgs()
+	filterList.Add("invalidFilter", serviceName)
+
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	_, err := service.GetService(filterList)
+
+	assert.Error(s.T(), err, "Error response from daemon: {\"message\":\"Invalid filter 'invalidFilter'\"}")
+}
+
+func (s *ServiceTestSuite) Test_GetTask_ReturnSwarmTask() {
+	serviceName := "docker-routing-mesh"
+	filterList := filters.NewArgs()
+	filterList.Add("name", serviceName)
+
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	swarmService, err := service.GetService(filterList)
+
+	assert.NoError(s.T(), err)
+
+	tasksFilter := filters.NewArgs()
+	tasksFilter.Add("service", swarmService.ID)
+
+	swarmTask, err := service.GetTask(tasksFilter)
+
+	assert.NoError(s.T(), err)
+	assert.IsType(s.T(), []swarm.Task{}, swarmTask)
+}
+
+func (s *ServiceTestSuite) Test_GetTask_ReturnError_InvalidFilter() {
+	serviceName := "docker-routing-mesh"
+	filterList := filters.NewArgs()
+	filterList.Add("name", serviceName)
+
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	swarmService, err := service.GetService(filterList)
+
+	assert.NoError(s.T(), err)
+
+	tasksFilter := filters.NewArgs()
+	tasksFilter.Add("invalidFilter", swarmService.ID)
+
+	_, err = service.GetTask(tasksFilter)
+
+	assert.Error(s.T(), err, "Error response from daemon: {\"message\":\"Invalid filter 'invalidFilter'\"}")
+}
+
+func (s *ServiceTestSuite) Test_GetDeploymentStatus_ReturnDeploymentStatus_ServiceNotExists() {
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	deploymentStatus, err := service.GetDeploymentStatus("my-service", "my-image:1.0.0")
+
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), "The my-service service was not found in the cluster.", deploymentStatus.Err)
+}
+
+func (s *ServiceTestSuite) Test_GetDeploymentStatus_ReturnDeploymentStatus_RunningDifferentImage() {
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	serviceName := "docker-routing-mesh"
+	deploymentStatus, err := service.GetDeploymentStatus(serviceName, "albertogviana/docker-routing-mesh:1.0.1")
+
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), deploymentStatus.ID)
+	assert.Equal(s.T(), serviceName, deploymentStatus.Name)
+	assert.Equal(s.T(), "The albertogviana/docker-routing-mesh:1.0.1 image was not deployed or not found in the current tasks running.", deploymentStatus.Err)
+}
+
+func (s *ServiceTestSuite) Test_GetDeploymentStatus_ReturnDeploymentStatus_ReturnDeploymentStatus() {
+	defer func() {
+		exec.Command("docker", "service", "scale", "docker-routing-mesh=1").Output()
+	}()
+
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	serviceName := "docker-routing-mesh"
+	image := "albertogviana/docker-routing-mesh:1.0.0"
+	deploymentStatus, err := service.GetDeploymentStatus(serviceName, image)
+
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), deploymentStatus.ID)
+	assert.Equal(s.T(), serviceName, deploymentStatus.Name)
+	assert.Equal(s.T(), uint64(1), *deploymentStatus.Replicas)
+	assert.Equal(s.T(), 1, deploymentStatus.RunningReplicas)
+	assert.Equal(s.T(), 0, deploymentStatus.FailedReplicas)
+	assert.Nil(s.T(), deploymentStatus.UpdateStatus)
+
+	exec.Command("docker", "service", "scale", "docker-routing-mesh=2").Output()
+	deploymentStatus2, err := service.GetDeploymentStatus(serviceName, image)
+
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), deploymentStatus2.ID)
+	assert.Equal(s.T(), serviceName, deploymentStatus2.Name)
+	assert.Equal(s.T(), uint64(2), *deploymentStatus2.Replicas)
+	assert.Equal(s.T(), 2, deploymentStatus2.RunningReplicas)
+	assert.Equal(s.T(), 0, deploymentStatus2.FailedReplicas)
+	assert.Nil(s.T(), deploymentStatus2.UpdateStatus)
+}
+
+func (s *ServiceTestSuite) Test_GetDeploymentStatus_ReturnDeploymentStatus_ReturnDeploymentStatusWithUpdateStatus() {
+	defer func() {
+		removeTestServices()
+		createTestServices()
+	}()
+
+	exec.Command("docker", "service", "update", "--image", "albertogviana/docker-routing-mesh:2.0.0", "--replicas", "2", "docker-routing-mesh").Output()
+
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	serviceName := "docker-routing-mesh"
+	deploymentStatus, err := service.GetDeploymentStatus(serviceName, "albertogviana/docker-routing-mesh:2.0.0")
+
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), deploymentStatus.ID)
+	assert.Equal(s.T(), serviceName, deploymentStatus.Name)
+	assert.Equal(s.T(), uint64(2), *deploymentStatus.Replicas)
+	assert.Equal(s.T(), 2, deploymentStatus.RunningReplicas)
+	assert.Equal(s.T(), 0, deploymentStatus.FailedReplicas)
+	assert.NotNil(s.T(), deploymentStatus.UpdateStatus)
+	assert.Equal(s.T(), swarm.UpdateStateCompleted, deploymentStatus.UpdateStatus.State)
+	assert.Equal(s.T(), "update completed", deploymentStatus.UpdateStatus.Message)
+}
+
+func (s *ServiceTestSuite) Test_GetDeploymentStatus_ReturnDeploymentStatus_ReturnDeploymentStatusWithUpdateStatusFailed() {
+	defer func() {
+		removeTestServices()
+		createTestServices()
+	}()
+
+	exec.Command("docker", "service", "update", "--image", "albertogviana/docker-routing-mesh:error", "--replicas", "2", "docker-routing-mesh").Output()
+
+	service := NewService(DockerHost, DockerAPIVersion, map[string]string{})
+	serviceName := "docker-routing-mesh"
+	deploymentStatus, err := service.GetDeploymentStatus(serviceName, "albertogviana/docker-routing-mesh:error")
+
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), deploymentStatus.ID)
+	assert.Equal(s.T(), serviceName, deploymentStatus.Name)
+	assert.Equal(s.T(), uint64(2), *deploymentStatus.Replicas)
+	assert.Equal(s.T(), 0, deploymentStatus.RunningReplicas)
+
+	failedReplicas := false
+	if deploymentStatus.FailedReplicas > 0 {
+		failedReplicas = true
+	}
+
+	assert.True(s.T(), failedReplicas)
+	assert.NotNil(s.T(), deploymentStatus.UpdateStatus)
+	assert.Equal(s.T(), swarm.UpdateStatePaused, deploymentStatus.UpdateStatus.State)
+}
+
+// Util
+
+func createTestServices() {
+	createTestService("docker-routing-mesh", []string{}, "", "albertogviana/docker-routing-mesh:1.0.0")
+}
+
+func createTestService(name string, labels []string, mode string, image string) {
+	args := []string{"service", "create", "--name", name}
+	for _, v := range labels {
+		args = append(args, "-l", v)
+	}
+	if len(mode) > 0 {
+		args = append(args, "--mode", "global")
+	}
+	args = append(args, image)
+	exec.Command("docker", args...).Output()
+}
+
+func removeTestServices() {
+	removeTestService("docker-routing-mesh")
+}
+
+func removeTestService(name string) {
+	exec.Command("docker", "service", "rm", name).Output()
+}
